a1fff7830b2497c89907af87a5d887887749ce45
leolpaz <leonardolpaz95@gmail.com>
2021-03-03 09:24:00 -0300

    Start fixing mistakes pointer by rubocop

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,15 +1,15 @@
 def bubble_sort(arr)
   i = 0
   ischanged = false
-  while i < arr.length-1
-    if arr[i] > arr[i+1]
-      arr[i], arr[i+1] = arr[i+1], arr[i]
+  while i < arr.length - 1
+    if arr[i] > arr[i + 1]
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
       ischanged = true
-      i+=1
+      i += 1
     else
-      i+=1
+      i += 1
     end
-    if i == arr.length-1 and ischanged == true
+    if i == arr.length - 1 and ischanged == true
       i = 0
       ischanged = false
     end
@@ -17,21 +17,21 @@ def bubble_sort(arr)
   print arr
 end
 
-a = [1,6,2,3,7,9,0,10,44,65,93,41,50,1000,12,11,4]
+a = [1, 6, 2, 3, 7, 9, 0, 10, 44, 65, 93, 41, 50, 1000, 12, 11, 4]
 bubble_sort(a)
 
 def bubble_sort_by(arr)
   i = 0
   ischanged = false
-  while i < arr.length-1
-    if yield(arr[i], arr[i+1]) > 0
-      arr[i], arr[i+1] = arr[i+1], arr[i]
+  while i < arr.length - 1
+    if yield(arr[i], arr[i + 1]).positive?
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
       ischanged = true
-      i+=1
+      i += 1
     else
-      i+=1
+      i += 1
     end
-    if i == arr.length-1 and ischanged == true
+    if i == arr.length - 1 and ischanged == true
       i = 0
       ischanged = false
     end
@@ -39,10 +39,6 @@ def bubble_sort_by(arr)
   print arr
 end
 
-bubble_sort_by(["hi","hello","hey", "heeeey", "hiii", "hellooooooo", "hiii"]) do |left,right|
- left.length - right.length
+bubble_sort_by(%w[hi hello hey heeeey hiii hellooooooo hiii]) do |left, right|
+  left.length - right.length
 end
-
-  
-
-
